8492cf46c7e183cd5a71711573a22d0e0dfc4902
danizaghian <dani.zaghian@gmail.com>
2018-10-16 10:48:47 -0700

    adds v-for on the review model

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <h2>{{ secondMessage }}</h2>
+    <li v-for="review in reviews">{{ review.rating }} STARS - {{ review.reviewer }}: {{ review.text }}</li>
   </div>
 </template>
 
@@ -13,7 +13,28 @@ export default {
   data: function() {
     return {
       message: "Hello world!",
-      secondMessage: "This is my second message"
+      reviews: [
+      	{ 
+          text: "Vue.js is the best thing since sliced bread!",
+          rating: 5,
+          reviewer: "Burton Wax"
+        },
+        { 
+          text: "I love Vue.js, especially when I'm programming!",
+          rating: 4,
+          reviewer: "Emily Post"
+        },
+        { 
+          text: "Vue.js is waaaaay better than Chinese Checkers.",
+          rating: 2,
+          reviewer: "Angela Pearson"
+        },
+        { 
+          text: "Vue.js so terrible, what am I doing?",
+          rating: 1,
+          reviewer: "Ace the Cat"
+        }
+      ]
     };
   },
   created: function() {},
